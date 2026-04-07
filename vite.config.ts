f5e0f454dedc13734bab16ef9132f836fc0d516e
devlabalaba <prasojotegar@gmail.com>
2026-04-07 09:19:14 +0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,16 +4,19 @@ import path from "path";
 import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: './', // ⬅️ WAJIB untuk Cloudflare Pages biar path tidak error
-
-  build: {
-    outDir: 'dist', // default Vite, tapi kita pertegas
-    emptyOutDir: true
-  },
-
+export default defineConfig(({ mode }) => ({
   server: {
-    port: 5173,
-    open: true
-  }
-});
+    host: "::",
+    port: 8080,
+    hmr: {
+      overlay: false,
+    },
+  },
+  plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+    dedupe: ["react", "react-dom", "react/jsx-runtime", "react/jsx-dev-runtime", "@tanstack/react-query", "@tanstack/query-core"],
+  },
+}));
