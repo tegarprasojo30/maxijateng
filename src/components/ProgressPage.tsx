09f021b1ae309c0cf6e23ff75e60c71ec738e929
devlabalaba <prasojotegar@gmail.com>
2026-04-09 14:54:55 +0700

    Update ProgressPage.tsx

--- src/components/ProgressPage.tsx
+++ src/components/ProgressPage.tsx
@@ -199,7 +199,7 @@ function GrafikTiles({ data }: { data: GrafikData }) {
                     <Cell key={index} fill={PIE_COLORS[index]} />
                   ))}
                 </Pie>
-                <Tooltip formatter={(value: number) => `${(percent * 100).toFixed(2)}%`}/>
+                <Tooltip formatter={(percent: number) => `${(percent * 100).toFixed(2)}%`}/>
                 <Legend />
               </PieChart>
             </ResponsiveContainer>
