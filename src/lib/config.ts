28f6e283acbaf485f4a9258ab970d6e139c42ae7
devlabalaba <prasojotegar@gmail.com>
2026-05-06 08:40:31 +0700

    Update config.ts

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,2 +1,2 @@
 // Paste URL Google Apps Script Web App di sini (format: https://script.google.com/macros/s/.../exec)
-export const KONFIRM_ANOMALI_URL = '';
+export const KONFIRM_ANOMALI_URL = 'https://script.google.com/macros/s/AKfycbzsrcK9XEQYHrbi-mA2_d0wRRCu5Po09kagmGsPm0yyD5XtwYC1BgPT7EkjkVsvtJba/exec';
